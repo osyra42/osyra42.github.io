b5fa4138f3d9cf7adbc96c5ef348a43265763de4
osyra42 <wchiado30@gmail.com>
2024-11-09 10:29:40 -0500

    fixed div issue

--- assets/right-sidebar.js
+++ assets/right-sidebar.js
@@ -8,15 +8,16 @@ document.getElementById("right-sidebar").innerHTML = `
 
 <h3>Project Links</h3>
 <div class="project-links">
-  <a href="index.html" class="nav-link"><i class="fas fa-home"></i>Home</a>
-  <a href="my_books.html" class="nav-link"><i class="fas fa-book"></i>My Books</a>
-  <a href="old/commissions.html" class="nav-link">X <i class="fas fa-shopping-cart"></i>Commissions</a>
-  <a href="minecraft.html" class="nav-link"><i class="fas fa-server"></i>Minecraft Server</a>
-  <a href="old/vanity_bot.html" class="nav-link">X <i class="fas fa-robot"></i>Vanity Bot</a>
-  <a href="contact_me.html" class="nav-link"><i class="fa-solid fa-address-card"></i>Contact Me</a>
-  <hr />
-  <a href="backrooms/index.html" class="nav-link"><i class="fas fa-door-open"></i>Backrooms</a>
-  <a href="bug_tracker.html" class="nav-link"><i class="fas fa-bug"></i>Bug Tracker</a>
+  <a title="Click to return to the home page" href="index.html" class="nav-link"><i class="fas fa-home"></i>Home</a>
+  <a title="Click to the page with my books" href="my_books.html" class="nav-link"><i class="fas fa-book"></i>My Books</a>
+  <a title="Click to submit a commission request" href="old/commissions.html" class="nav-link">X <i class="fas fa-shopping-cart"></i>Commissions</a>
+  <a title="Click to view information about the Minecraft server" href="minecraft.html" class="nav-link"><i class="fas fa-server"></i>Minecraft Server</a>
+  <a title="Click to view information about the Vanity" href="old/vanity_bot.html" class="nav-link">X <i class="fas fa-robot"></i>Vanity Bot</a>
+  <a title="Click to contact me or view basic information about me" href="about_me.html" class="nav-link"><i class="fa-solid fa-address-card"></i>About Me</a>
+
+  <a title="Click to view bugs or submit a bug report" href="bug_tracker.html" class="nav-link"><i class="fas fa-bug"></i>Bug Tracker</a>
+  <hr>
+   <a title="Click to view notes on new project ideas" href="backrooms/index.html" class="nav-link"><i class="fas fa-door-open"></i>Backrooms</a>
 </div>
 
 <div class="pth">
