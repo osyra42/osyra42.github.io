e35a4c21c1c771154ef93499075eb968aab20d63
osyra42 <wchiado30@gmail.com>
2024-11-14 06:14:39 -0500

    update a few things

--- assets/right-sidebar.js
+++ assets/right-sidebar.js
@@ -9,38 +9,37 @@ document.getElementById("right-sidebar").innerHTML = `
 <h3>Project Links</h3>
 <div class="project-links">
   <a title="Click to return to the home page" href="index.html" class="nav-link"><i class="fas fa-home"></i>Home</a>
+
   <a title="Click to the page with my books" href="my_books.html" class="nav-link"
-    ><i class="fas fa-book"></i>My Books</a
-  >
+    ><i class="fas fa-book"></i>My Books</a>
+
+  <a title="Click to view information about the Vanity" href="vanity.html" class="nav-link"
+    ><i class="fas fa-robot"></i>Vanity Bot</a>
 
   <a title="Click to view information about the Minecraft server" href="minecraft.html" class="nav-link"
-    ><i class="fas fa-server"></i>Minecraft Server</a
-  >
-  <a title="Click to view information about the Vanity" href="vanity.html" class="nav-link"
-    ><i class="fas fa-robot"></i>Vanity Bot</a
-  >
-  <a title="Click to submit a commission request" href="old/commissions.html" class="nav-link"
-    >X <i class="fas fa-shopping-cart"></i>Commissions</a
-  >
-  <a title="Click to contact me or view basic information about me" href="about_me.html" class="nav-link"
-    ><i class="fa-solid fa-address-card"></i>About Me</a
-  >
+    ><i class="fas fa-server"></i>Minecraft Server</a>
 
-  <a title="Click to view bugs or submit a bug report" href="bug_tracker.html" class="nav-link"
-    ><i class="fas fa-bug"></i>Bug Tracker</a
-  >
+  <a title="Click to submit a commission request" href="commissions.html" class="nav-link"
+    ><i class="fas fa-shopping-cart"></i>Commissions</a>
+
+  <a title="Click to contact me or view basic information about me" href="about_me.html" class="nav-link"
+    ><i class="fa-solid fa-address-card"></i>About Me</a>
+
   <hr />
+
   <a title="Click to view notes on new project ideas" href="backrooms/index.html" class="nav-link"
-    ><i class="fas fa-door-open"></i>Backrooms</a
-  >
+    ><i class="fas fa-door-open"></i>Backrooms</a>
+
+  <a title="Click to view bugs or submit a bug report" href="backrooms/bug_tracker.html" class="nav-link"
+    ><i class="fas fa-bug"></i>Bug Tracker</a>
 </div>
 
 <div class="pth">
   <div>
     <hr />
-    <a href="privacy.html" class="nav-link pth-link"><i class="fas fa-shield-alt"></i> Privacy</a> |
-    <a href="terms.html" class="nav-link pth-link"><i class="fas fa-gavel"></i> Terms</a> |
-    <a href="help.html" class="nav-link pth-link"><i class="fas fa-question-circle"></i> Help</a>
+    <a target="_blank" href="legal/privacy_policy.txt" class="nav-link pth-link"><i class="fas fa-shield-alt"></i> Privacy</a> |
+    <a target="_blank" href="legal/terms_of_service.txt" class="nav-link pth-link"><i class="fas fa-gavel"></i> Terms</a> |
+    <a target="_blank" href="legal/help_and_faq.txt" class="nav-link pth-link"><i class="fas fa-question-circle"></i> Help</a>
   </div>
 </div>
 `;
