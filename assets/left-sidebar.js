2cda45661d6e6f8778fdec1c59955c2f23ebb314
osyra42 <22022820+osyra42@users.noreply.github.com>
2024-11-14 02:41:01 -0500

    more pages

--- assets/left-sidebar.js
+++ assets/left-sidebar.js
@@ -7,15 +7,7 @@ document.getElementById("left-sidebar").innerHTML = `
         <img src="images/${image}" alt="${title}" />
     </div>
     <h2>${title}</h2>
-    </div>
-    <div class="skills">
-    <h3>Skills</h3>
-    <div class="skill-icons">
-        <i class="fab fa-html5 skill-icon" title="HTML5"></i>
-        <i class="fab fa-js-square skill-icon" title="JavaScript"></i>
-        <i class="fab fa-css3-alt skill-icon" title="CSS3"></i>
-        <i class="fab fa-python skill-icon" title="Python"></i>
-        <i class="fa-solid fa-robot skill-icon" title="AI Prompting"></i>
-    </div>
 </div>
+
+
 `;
