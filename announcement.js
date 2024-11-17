e54ccde489f44b2c6e1fb317bdfe25cde08894c7
osyra42 <22022820+osyra42@users.noreply.github.com>
2024-11-17 07:37:36 -0500

    nounce

--- announcement.js
+++ announcement.js
@@ -1,3 +1,3 @@
 document.getElementById("announcement").innerHTML = `
-This website is under construction. Last update was <span style="color: black">2024 Nov 14 @ 03:00h</span>
+This website is under construction. Last update was <span style="color: black">2024 Nov 16 @ 21:00h</span>
 `;
