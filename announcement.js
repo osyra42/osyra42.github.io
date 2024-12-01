0173ee8a946d661f73d439c4b6b7ad06ba1df5f9
osyra42 <22022820+osyra42@users.noreply.github.com>
2024-11-30 21:15:58 -0500

    update about me

--- announcement.js
+++ announcement.js
@@ -1,6 +1,6 @@
 document.getElementById("announcement").innerHTML = `
-This website is under construction.
- <span style="color: lime">Desktop: 93% </span> ~ 
-   <span style="color: red">Mobile: 12% </span> ~ 
-<span style="color: black">Updated: 2024Nov20</span>
+Development... |
+ <span style="color: lime">Desktop: 94% </span> | 
+   <span style="color: red">Mobile: 14% </span> | 
+<span style="color: black">Updated: 2024Nov30</span>
 `;
