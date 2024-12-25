b6b553a41101e9d1f9fe0aadc8350506a7fd2b33
osyra42 <wchiado30@gmail.com>
2024-12-25 15:22:14 -0500

    update clutter

--- announcement.js
+++ announcement.js
@@ -1,6 +1,6 @@
 document.getElementById("announcement").innerHTML = `
 Development... |
- <span style="color: lime">Desktop: 94% </span> | 
-   <span style="color: red">Mobile: 14% </span> | 
-<span style="color: black">Updated: 2024Nov30</span>
+ <span style="color: lime">Desktop: 95% </span> | 
+   <span style="color: red">Mobile: 30% </span> | 
+<span style="color: black">Updated: 2024Dec25</span>
 `;
